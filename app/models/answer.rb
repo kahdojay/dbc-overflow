81636ba9f3aac5851d1f1c35a6cffa2c6b9af02d
Don J <kahdojay@gmail.com>
2015-01-15 17:00:44 -0500

    Add answer/user models.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
+
+  validates_presence_of :body
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :questions
+  has_many :answers
+
+  validates_presence_of :name, :password
+  validates_uniqueness_of :name
 end
