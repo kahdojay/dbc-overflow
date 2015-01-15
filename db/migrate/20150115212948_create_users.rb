e17dbd96f1427bf3b3119ac8d30dc11c191b4ded
Don J <kahdojay@gmail.com>
2015-01-15 16:36:02 -0500

    Add not null db validations to users.

--- db/migrate/20150115212948_create_users.rb
+++ db/migrate/20150115212948_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password_digest
+      t.string :name, null: false
+      t.string :password_digest, null: false
       t.timestamps
     end
   end
