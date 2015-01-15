d30f5f990fe79d3d547b3273dc30a61031339f49
Mike Horn <mhorn918@gmail.com>
2015-01-15 17:51:14 -0500

    Setup basic actions for answers_controller.rb

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,46 @@
+class AnswersController < ApplicationController
+
+  def index
+    @answers = Answer.all
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    question = Question.new(params[:question_id])
+    question.answers.create(answers_params)
+    redirect_to questions_path
+    render 'new'
+  end
+
+  def edit
+    @answer = Answer.find(params[:id])
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+    if @answer.update_attributes(params[:answer])
+      redirect_to questions_path
+    else
+      render 'edit'
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+  end
+
+  def destroy
+
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    redirect_to questions_path
+  end
+
+  def answers_params
+    params.require(:answer).permit(:body)
+
+  end
+
+end
