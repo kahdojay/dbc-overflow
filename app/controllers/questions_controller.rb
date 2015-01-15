2f170646bda51dad87b3ccedad4f0428d1c14810
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-15 17:58:09 -0500

    Controllers: created create action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,17 @@ class QuestionsController < ApplicationController
   def new
   end
 
+  def create
+    params[:question][:user_id] = 1
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to question_path(@question.id)
+    else
+      #error_message?
+      redirect_to :back
+    end
+  end
+
   private
     def question_params
       params.require(:question).permit(:title, :body, :user_id)
