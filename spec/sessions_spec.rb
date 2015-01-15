37af54dffc720917d60d0119ea3214be261128d4
Don J. Kahn & <pair+kahdojay.kvnkwn@devbootcamp.com>
2015-01-15 18:17:19 -0500

    Setup first session test pseudocode.

--- spec/sessions_spec.rb
+++ spec/sessions_spec.rb
@@ -2,8 +2,21 @@ require 'spec_helper'
 
 describe SessionsController do
   describe '#login' do
-    it 'creates a session upon login with valid credentials'
-    it 'redirects a user with invalid credentials to the login page'
-    it 'displays an error message for users with invalid credentials'
+    context 'with valid credentials' do
+      before :each do
+        user = create(:user)
+      end
+
+      it 'creates a session upon login' do
+        # send user login params to create_session
+        # expect sessoin[:id] to be user.id
+        expect(session[:id]).to eq(user.id)
+      end
+    end
+
+    context 'without valid credentials' do
+      xit 'redirects a user with invalid credentials to the login page'
+      xit 'displays an error message for users with invalid credentials'
+    end
   end
 end
