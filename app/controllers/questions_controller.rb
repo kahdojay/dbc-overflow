818386cc66bcc519f620106f325c8c9483adf0d0
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-15 17:44:24 -0500

    Controllers: created strong params

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,8 @@ class QuestionsController < ApplicationController
   def new
   end
 
+  private
+    def question_params
+      params.require(:question).permit(:title, :body, :user_id)
+    end
 end
