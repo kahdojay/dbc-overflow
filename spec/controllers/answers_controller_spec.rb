665b5638558ec040529b8cde28c27904ebae3569
Mike Horn <mhorn918@gmail.com>
2015-01-16 15:00:11 -0500

    Failing test removed for rendering new template

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 
 describe AnswersController do
 
-
-  describe 'GET #show' do
-
-    it 'renders the :answer#show template' do
+  describe '#show' do
+    it 'renders the answer#show template' do
       question = create(:question)
       answer = create(:answer, question: question)
       get :show, id: question, question_id: question.id
       expect(response).to render_template :show
     end
   end
+
 
 end
