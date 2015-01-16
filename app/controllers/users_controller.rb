359cf3c8c45413f4c2e6cd25df6d058fa6b1e7d4
HaileyR <hangyeolryu@gmail.com>
2015-01-16 15:20:04 -0800

    Add user create method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,18 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:id] = @user.id
+			redirect_to :root
+		else
+			render :new
+		end
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :password, :password_confirmation)
+	end
+
 end
