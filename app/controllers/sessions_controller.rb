dbe5064c2ddc7e6ebc7ad4a8ee7225f8ab84e066
Don J <kahdojay@gmail.com>
2015-01-16 00:50:22 -0500

    Refactor login rspec tests.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: login_params[:name])
+    session[:id] = user.id
+    redirect_to root_url
   end
 
   def logout
   end
 
   def login_params
-    params.require(:session).permit(:name, :password)
+    params.require(:user).permit(:name, :password)
   end
 end
