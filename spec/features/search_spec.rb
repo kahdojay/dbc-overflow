24ffd31d9ab7eb24d45b99f9fa8175aeeaabf249
HaileyR <hangyeolryu@gmail.com>
2015-01-16 12:16:40 -0800

    Fix test.

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 feature 'Search question' do
   scenario "search a keyword" do
+    Question.create({user_id: 1, title:'abcdefg', body: 'This is a test question'})
     visit root_path
-    find('#search_form').fill_in 'q', with: 'Aliquam'
+    find('#search_form').fill_in 'q', with: 'abcdefg'
     click_button 'Search'
-
-    expect(response.body).to have_content 'Aliquam'
-  end
+    expect(page.body).to have_content 'abcdefg'
+    end
 end
