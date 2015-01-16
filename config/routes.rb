9c5a228739acc1136246f52d5206c78cb5288fc0
Don J <kahdojay@gmail.com>
2015-01-16 01:11:57 -0500

    Define logout route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#logout'
   resources :questions do
     resources :answers
   end
