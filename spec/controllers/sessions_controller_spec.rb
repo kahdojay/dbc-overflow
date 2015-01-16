cd2b0f07131197d014a798e122a0f47d2d98cce9
Don J <kahdojay@gmail.com>
2015-01-16 00:27:50 -0500

    Prepare session login test.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,22 +1,25 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe SessionsController do
   describe '#login' do
     context 'with valid credentials' do
       before :each do
-        user = create(:user)
       end
 
       it 'creates a session upon login' do
+        user = FactoryGirl.create(:user)
+
         # send user login params to session#create
+        post :create, user: user.attributes
+
         # expect session[:id] to be user.id
         expect(session[:id]).to eq(user.id)
       end
     end
 
-    context 'without valid credentials' do
-      xit 'redirects a user with invalid credentials to the login page'
-      xit 'displays an error message for users with invalid credentials'
-    end
+    # context 'without valid credentials' do
+    #   xit 'redirects a user with invalid credentials to the login page'
+    #   xit 'displays an error message for users with invalid credentials'
+    # end
   end
 end
