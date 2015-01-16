0c564000d12401190f190a9d3aa233003d5fb395
Peter Jahnes <peter.jahnes@gmail.com>
2015-01-16 14:54:02 -0500

    creating the logic for the create controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,8 +26,12 @@ class UsersController < ApplicationController
 		end
 	end
 
-	def user_params
-		params.require(:user).permit(:name, :password, :password_confirmation)
-	end
+	private
+
+		def user_params
+			params.require(:user).permit(:name, :password, :password_confirmation)
+		end
+
+
 
 end
