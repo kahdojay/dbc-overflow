be639f86c4b54270c142a20cf10af4b2ccc5a87e
Peter Jahnes <peter.jahnes@gmail.com>
2015-01-16 20:58:26 -0500

    fixing test for post create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -30,8 +30,8 @@ describe UsersController do
 	describe 'POST #create' do
 
 		before :each do
-			@user = FactoryGirl.create( :user )
-			post :create
+			@user = {name: "butch", password: "123", password_confirmation: "123"}
+			post :create, user: @user
 		end
 
 		it "redirects to #root" do
@@ -83,12 +83,5 @@ describe UsersController do
 		end
 
 	end
-
-	private
-
-		def user_params
-			params.require(:user).permit(:name, :password, :password_confirmation)
-    end
-
-
+	
 end
