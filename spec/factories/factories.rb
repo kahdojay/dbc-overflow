ba01bc530361df39214c79d0a766ac63afff1887
Don J. Kahn & <pair+kahdojay.kvnkwn@devbootcamp.com>
2015-01-17 13:53:55 -0500

    Add questions factory to spec environment.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,4 +4,10 @@ FactoryGirl.define do
     name { Faker::Internet.user_name }
     password { Faker::Internet.password }
   end
+
+  factory :question do
+    user_id { rand(User.count) }
+    title { Faker::Lorem.sentence }
+    body { Faker::Lorem.paragraph }
+  end
 end
