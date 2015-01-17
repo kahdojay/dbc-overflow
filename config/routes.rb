f0e05a2ff1e9febb3e19bf4cc7d5b408e80c4292
Don J <kahdojay@gmail.com>
2015-01-16 19:22:36 -0500

    Revert "Setup comments and votes resourceful routes."
    This reverts commit e799bfab71e046fb5b5d890105424b369e0d22ec.

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,10 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#logout'
-  get '/search', to: 'welcome#search'
 
   resources :users
   resources :questions do
     resources :answers
   end
-
-  resources :comments
-  resources :votes
+  get '/search', to: 'welcome#search'
 end
