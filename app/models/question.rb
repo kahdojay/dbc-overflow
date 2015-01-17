fe0ec38a7814f6e001a78ad720ecfda0ad357d5b
Don J <kahdojay@gmail.com>
2015-01-16 19:14:44 -0500

    Add/update answer/question/user/vote models (rev2).

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
   has_many :comments, as: :commentable
+  has_many :votes, as: :votable
 
   validates_presence_of :body
   validates_presence_of :title
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
   has_many :comments
+  has_many :votes
 
   validates_presence_of :name, :password
   validates_uniqueness_of :name
