f2d1939e7c1160034c4ef87db311b7242d6f4f9e
Don J. Kahn & <pair+kahdojay.kvnkwn@devbootcamp.com>
2015-01-17 14:30:48 -0500

    Remove context from spec, doesn't matter the context.

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,28 +2,26 @@ require 'rails_helper'
 
 describe VotesController do
   describe '#create' do
-    context 'on the welcome#index page' do
-      before :each do
-        @user = FactoryGirl.create(:user)
-        @question = FactoryGirl.create(:question)
-        @question.votes.create(user_id: @user.id, upvote: true)
-      end
+    before :each do
+      @user = FactoryGirl.create(:user)
+      @question = FactoryGirl.create(:question)
+      @question.votes.create(user_id: @user.id, upvote: true)
+    end
 
-      it 'creates a vote record with the correct user_id' do
-        expect(@question.votes.last.user_id).to eq(@user.id)
-      end
+    it 'creates a vote record with the correct user_id' do
+      expect(@question.votes.last.user_id).to eq(@user.id)
+    end
 
-      it 'creates a vote record with the correct question_id' do
-        expect(@question.votes.last.votable_id).to eq(@question.id)
-      end
+    it 'creates a vote record with the correct question_id' do
+      expect(@question.votes.last.votable_id).to eq(@question.id)
+    end
 
-      it 'creates a vote record with the upvote attribute as true' do
-        expect(@question.votes.last.upvote).to eq(true)
-      end
+    it 'creates a vote record with the upvote attribute as true' do
+      expect(@question.votes.last.upvote).to eq(true)
+    end
 
-      it 'adds the vote to the votes table in the db' do
-        expect{@question.votes.create(user_id: @user.id, upvote: true)}.to change{Vote.count}.by(1)
-      end
+    it 'adds the vote to the votes table in the db' do
+      expect{@question.votes.create(user_id: @user.id, upvote: true)}.to change{Vote.count}.by(1)
     end
   end
 end
