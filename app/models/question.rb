cf81a48e8c977fb96d69a9919c7f34fafc7b965c
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-17 16:07:21 -0500

    Models: created create_tags method

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,8 +9,13 @@ class Question < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :user_id
 
+
   def vote_count
     self.votes.where(upvote: true).count - self.votes.where(upvote: false).count
+  end
+
+  def create_tags(string)
+    string.split(',').each{ |tag| self.tags.create(name: tag)}
   end
 
   def self.search(words)
