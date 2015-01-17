216514eddb713d2fa1fef5e63f0077b91ec5e814
HaileyR <hangyeolryu@gmail.com>
2015-01-17 13:41:30 -0800

    Fix comment create.

--- app/controllers/concerns/comments_controller.rb
+++ app/controllers/concerns/comments_controller.rb
@@ -2,9 +2,22 @@ class CommentsController < ApplicationController
   include AuthsHelper
 
   def create
-    question = Question.find(params[:question_id])
-    answer = question.answers.create(answers_params)
-    redirect_to question_path(question, "answers" => answer.id)
+    @question = Question.find(q_params[:question_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to question_path(@question)
+    else
+      #error_message?
+      render :new
+    end
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commentable_id, :commentable_type, :body, :user_id)
+    end
+    def q_params
+      params.require(:comment).permit(:question_id)
+    end
 
 end
