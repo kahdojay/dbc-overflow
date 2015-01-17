f3a37be16bf67e8a4fc9e7e6ad45c633189026e4
HaileyR <hangyeolryu@gmail.com>
2015-01-17 11:26:22 -0800

    Add validation on comment model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  validates_presence_of :body
+  validates_presence_of :commentable_id
+  validates_presence_of :commentable_type
+  validates_presence_of :user_id
+
 end
