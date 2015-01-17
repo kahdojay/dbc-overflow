165d000acf989bb63c5611834d34594d452a2183
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-17 10:03:49 -0500

    Rspec: wrote pending tests for update/destroy

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -38,7 +38,18 @@ describe QuestionsController do
     end
   end
 
-  describe 'GET #edit' do
+  xdescribe 'GET #edit' do
+    it "assigns the equested question to @question" do
+      question = create(:question)
+      get :edit, id: question
+      expect(assigns(:question)).to eq question
+    end
+
+    it "renders the #edit template" do
+      question = create(:question)
+      get :edit, id: question
+      expect(response).to render_template :edit
+    end
   end
 
   describe 'POST #create' do
@@ -68,10 +79,61 @@ describe QuestionsController do
     end
   end
 
-  describe 'PATCH #update' do
+  xdescribe 'PATCH #update' do
+    before :each do
+      @question = create(:question,
+      body: "this is the body",
+      title: "Django",
+      user_id: "1"
+      )
+    end
+    context "with valid attributes" do
+      it "locates the requested question" do
+        patch :update, id: @question, question: attributes_for(:question)
+        expect(assigns(:question)).to eq(@question)
+      end
+      it "changes @questions attributes" do
+        patch :update, id: @question, body: "different!", title: "Django"
+        @question.reload
+        expect(@question.body).to eq("different!")
+        expect(@question.title).to eq("Django")
+      end
+      it "redirects to the updated question" do
+        patch :update, id: @question, question: attributes_for(:question)
+        expect(response).to redirect_to @question
+      end
+      context "with invalid attributes" do
+        it "does not change the attributes" do
+          patch :update, id: @question, question: attributes_for(:question, body: nil, title: "Bazinga")
+          @question.reload
+          expect(@question.title).not_to eq("Bazinga")
+          expect(@question.body).to eq("this is the body")
+        end
+
+        it "re-renders the :edit template" do
+          patch :update, id: @question, question: attributes_for(:invalid_question)
+          expect(response).to render_template :edit
+        end
+      end
+    end
+
   end
 
-  describe 'DELETE #destroy' do
+  xdescribe 'DELETE #destroy' do
+    before :each do
+      @quesiton = create(:question)
+    end
+
+    it "deletes the question from the database" do
+      expect{
+      delete :destroy, id: @question
+      }.to change(Question, :count).by(-1)
+    end
+
+    it "renders the :index template" do
+      delete :destroy, id: @question
+      expect(response).to redirect_to questions_path
+    end
   end
 
 end
