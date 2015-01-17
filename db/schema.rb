88471392a497d6bfd839d0d1b0f48a36e2911257
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-17 14:53:09 -0500

    Migration: created tags table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116235403) do
+ActiveRecord::Schema.define(version: 20150117194950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(version: 20150116235403) do
     t.text     "body",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name"
+    t.integer "question_id"
   end
 
   create_table "users", force: :cascade do |t|
