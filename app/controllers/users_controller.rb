8f704c27625c25230d9c52ef0a98c5b9b0d4adb4
Peter Jahnes <peter.jahnes@gmail.com>
2015-01-16 23:38:50 -0500

    adding user edit and destroy actions in the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,32 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def update
+			@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			render :show
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+ 		if @user.id = session[:id]
+			if @user.destroy
+				render :root
+			else
+				render :back
+ 			end
+ 		else
+			if @user.destroy
+				render :index
+			else
+				render :back
+			end
+		end
+	end 
+
 	private
 
 		def user_params
