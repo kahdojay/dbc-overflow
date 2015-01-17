dbff34668f01511e7962a4f6af02c55ee1ea1d69
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-17 14:02:39 -0500

    Controllers: redirected destroy action to root path

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -40,7 +40,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
-    redirect_to questions_path
+    redirect_to root_path
   end
 
   private
