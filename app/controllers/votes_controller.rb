1326fe2dde3a464a9fd31c909d6213517c3d3e70
Don J. Kahn & <pair+kahdojay.kvnkwn@devbootcamp.com>
2015-01-17 14:12:37 -0500

    Add votes#create action for basic upvotes.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,15 @@
 class VotesController < ApplicationController
+  def create
+    vote = Vote.new(vote_params)
+    if vote.save
+      redirect_to root_path
+    else
+      set_error('Login to vote.')
+      redirect_to '/login'
+    end
+  end
+
+  def vote_params
+    params.require(:vote).permit(:user_id, :votable_type, :votable_id, :upvote)
+  end
 end
