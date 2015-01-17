7654900d211aad745402318f0f18244a56a52c8c
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-17 12:10:57 -0500

    Controllers: created update action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -28,6 +28,15 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
   private
     def question_params
       params.require(:question).permit(:title, :body, :user_id)
