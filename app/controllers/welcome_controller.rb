6c0093d61af2fba0383e9cca7a882d6c85d4c0f3
Peter Jahnes <peter.jahnes@gmail.com>
2015-01-18 00:28:27 -0500

    refactoring route and search routes so that they are more conventiony

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,8 @@
 class WelcomeController < ApplicationController
-  def index
-    @questions = Question.all.order('created_at DESC')
-  end
-
-  def show
-
-  end
 
   def search
     @questions = Question.search(params[:q])
-    render 'index'
+    render "questions/index.html.erb"
   end
+  
 end
