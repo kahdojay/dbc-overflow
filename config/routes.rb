ccde872b1451b39c0c7b69961a4d80b2b3027eb3
Don J <kahdojay@gmail.com>
2015-01-18 13:04:55 -0500

    Move root to questions#index, reorganize routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'questions#index'
 
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
@@ -7,13 +7,12 @@ Rails.application.routes.draw do
   get '/search', to: 'welcome#search'
   get '/signup', to: 'users#new'
 
-  resources :users
-  resources :comments, only: [:create, :edit, :destroy, :update]
-
   post 'votes/upvote', to: 'votes#upvote'
   post 'votes/downvote', to: 'votes#downvote'
+
+  resources :users
+  resources :comments, only: [:create, :edit, :destroy, :update]
   resources :questions do
     resources :answers
-
   end
 end
