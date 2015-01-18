50c9bf56f1a818b2eccb9a9e07810965a2bbc082
Don J <kahdojay@gmail.com>
2015-01-17 21:33:14 -0500

    Add invalid_answer factory.

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
         user_id 1
         question_id 1
         body "Check out the Rails Guides and you'll find the answer!"
+
+        factory :invalid_answer do
+          body nil
+        end
     end
 end
