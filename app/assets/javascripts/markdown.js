6953677d6c6dd43b9584fcbf52b143b0fe17165c
Don J. Kahn & <pair+kahdojay.kvnkwn@devbootcamp.com>
2015-01-19 15:18:33 -0500

    Remove css rules, write italicize/bold rules.

--- app/assets/javascripts/markdown.js
+++ app/assets/javascripts/markdown.js
@@ -1,13 +1,58 @@
 function MarkdownWidget(sourceId, previewDiv) {
   this.newQuestionId = sourceId
   this.preview =  $(previewDiv)
+  this.getQuestion()
 }
 
 MarkdownWidget.prototype.getQuestion = function() {
   this.newQuestion = $(this.newQuestionId).find('textarea').val()
 }
+
 MarkdownWidget.prototype.renderMarkdown = function() {
   this.getQuestion()
-  $('#question-preview').empty()
-  $('#question-preview').html(this.newQuestion)
-  }
+  this.preview.empty()
+
+  this.wordArray = this.newQuestion.split(/\s/)
+  this.tempArray = []
+  this.applyRules()
+
+  var that = this
+  this.wordArray.forEach(function(word) {
+    that.preview.append(word + ' ')
+  })
+}
+
+MarkdownWidget.prototype.applyRules = function() {
+  this.bold()
+  this.italicize()
+  // this.underline()
+}
+
+MarkdownWidget.prototype.italicize = function() {
+  var that = this
+  this.wordArray.forEach(function(word) {
+    var italicsAsterisk = new RegExp(/(\*)(.+)(\*)/)
+    var italicsUnderscore = new RegExp(/(\_)(.+)(\_)/)
+    if (italicsAsterisk.test(word) || italicsUnderscore.test(word)) {
+      word = word.replace(/^./, '<em>');
+      word = word.replace(/.$/, '</em>');
+    }
+    that.tempArray.push(word)
+  })
+  this.wordArray = this.tempArray
+  this.tempArray = []
+}
+
+MarkdownWidget.prototype.bold = function() {
+  var that = this
+  this.wordArray.forEach(function(word) {
+    var bold = new RegExp(/(\*\*)(.+)(\*\*)/)
+    if (bold.test(word)) {
+      word = word.replace(/^../, '<strong>');
+      word = word.replace(/..$/, '</strong>');
+    }
+    that.tempArray.push(word)
+  })
+  this.wordArray = this.tempArray
+  this.tempArray = []
+}
