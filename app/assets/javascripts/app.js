6505582d6cd8d05553459fa2e5d43e40dff5e746
Don J. Kahn & <pair+kahdojay.kvnkwn@devbootcamp.com>
2015-01-19 18:20:04 -0500

    Push for transfer to another repo.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,22 @@
+
 $(document).ready(function() {
-  widget = new MarkdownWidget('#new-question', '#question-preview')
-  $('#new-question').on('keyup', function(event){
-    widget.renderMarkdown()
-  })
+  // widget = new MarkdownWidget('#new-question', '#question-preview')
+  // $('#new-question').on('keyup', function(event){
+  //   widget.renderMarkdown()
+  // })
+
+  function Editor(input, preview) {
+    // var markdown = require( "markdown" ).markdown;
+    this.update = function () {
+      preview.innerHTML = markdown.toHTML(input.value);
+    };
+    input.editor = this;
+    this.update();
+  }
+
+  // var $ = function (id) { return document.getElementById(id); };
+  new Editor($("#new-question")[0], $("question-preview")[0]);
+
 })
+
+
